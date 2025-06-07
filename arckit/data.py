d29be094a4035b30a6b64969e47e329ce3cff4c9
Adarsh <70708225+madarshb19@users.noreply.github.com>
2025-06-07 17:02:35 +0530

    Update data.py for ARC25

--- arckit/data.py
+++ arckit/data.py
@@ -240,7 +240,7 @@ class TaskSet:
         
 
 def load_data() -> (TaskSet, TaskSet):
-    data = json.load(open(f"{os.path.dirname(__file__)}/arc1.json"))
+    data = json.load(open(f"{os.path.dirname(__file__)}/arc25.json"))
     train_tasks = []
     eval_tasks = []
     for id, task in data['train'].items():
@@ -255,7 +255,7 @@ def load_single(id: str) -> Task:
     """
     Load a single task from disk.
     """
-    data = json.load(open(f"{os.path.dirname(__file__)}/arc1.json"))
+    data = json.load(open(f"{os.path.dirname(__file__)}/arc25.json"))
     # task = data['train'][id]
     # return Task(id, task['train'], task['test'], 'train'
     if id.startswith('train'):
